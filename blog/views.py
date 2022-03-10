8d037d146bca5f9096f29652ed495ba1fe8b9e89
VladRadigin <angry.gamer.comi@gmail.com>
2022-03-11 00:07:25 +0600

    Listing blog posts on the Homepage

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,9 @@ class BlogView(generic.DetailView):
     model = Post
     template_name = 'blog.html'
 
-class HomeView(generic.TemplateView):
-    template_name = 'index.html'
-
 class AboutView(generic.TemplateView):
     template_name = 'about.html'
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-date_created')
+    template_name = 'index.html'
