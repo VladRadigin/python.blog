88164036efa989e71bbf99375ce388ad50e0b742
VladRadigin <angry.gamer.comi@gmail.com>
2022-03-13 01:06:13 +0600

    Adding a comment model system

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,16 @@
 from django.contrib import admin
 from .models import Post
+from .models import Comment
 
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'date_created', 'author')
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'post', 'created', 'active')
+    list_filter = ('active', 'created', 'updated')
+    search_fields = ('name', 'email', 'body')
+
 
 admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
